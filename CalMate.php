85473834ed2b0ee960c2a02250004b176041f220
ihabz <ihab.zeedia@stanford.edu>
2024-12-09 15:23:35 -0800

    initial template

---
+++ CalMate.php
@@ -0,0 +1,10 @@
+<?php
+namespace Stanford\CalMate;
+
+class CalMate extends \ExternalModules\AbstractExternalModule {
+    public function __construct() {
+        parent::__construct();
+        // Other code to run when object is instantiated
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Calendar Mate 
+
+This EM will pull the Stanford Email calendar and allow people to book an available time. 
